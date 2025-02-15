cc5e082b458e224df0d85ed772c6500f60b38cf7
sttk <sttk.xslet@gmail.com>
2025-02-15 15:48:55 +0900

    new!: added camelCaseWithOptions, updated camelCase to use it, and deprecated camelCaseWithSep/Keep

---
+++ src/main/java/com/github/sttk/stringcase/Options.java
@@ -0,0 +1,56 @@
+/*
+ * Options class.
+ * Copyright (C) 2025 Takayuki Sato. All Rights Reserved.
+ */
+package com.github.sttk.stringcase;
+
+/**
+ * Is a class that represents options which specifies the ways of case conversion of strings.
+ */
+public class Options {
+  /**
+   * Specifies whether to treat the beginning of a sequence of non-alphabetical characters as a
+   * word boundary.
+   */
+  public final boolean separateBeforeNonAlphabets;
+
+  /**
+   * Specifies whether to treat the end of a sequence of non-alphabetical characters as a word
+   * boundary.
+   */
+  public final boolean separateAfterNonAlphabets;
+
+  /**
+   * Specifies the set of characters to be treated as word separators and removed from the result
+   * string.
+   */
+  public final String separators;
+
+  /**
+   * Specifies the set of characters not to be treated as word separators and kept in the result
+   * string.
+   */
+  public final String keep;
+
+  /**
+   * The constructor which takes the arguments that specifies the ways of case conversion.
+   *
+   * @param separateBeforeNonAlphabets  The flag that specifies whether to treat the beginning of
+   *   a sequence of non-alphabetical characters as a word boundary.
+   * @param separateAfterNonAlphabets  The flag that specifies whether to treat the end of a
+   *   sequence of non-alphabetical characters as a word boundary.
+   * @param separators  The symbol characters to be treated as word separators and removed from
+   *   the result string.
+   * @param keep  The symbol characters tobe treated as word separators and kept in the result
+   *   string.
+   */
+  public Options(
+    boolean separateBeforeNonAlphabets, boolean separateAfterNonAlphabets,
+    String separators, String keep
+  ) {
+    this.separateBeforeNonAlphabets = separateBeforeNonAlphabets;
+    this.separateAfterNonAlphabets = separateAfterNonAlphabets;
+    this.separators = separators;
+    this.keep = keep;
+  }
+}
