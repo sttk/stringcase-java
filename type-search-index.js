dcb747e0bf31b846df7ac24b346167f4d2793151
sttk <sttk@users.noreply.github.com>
2025-02-17 09:55:07 +0000

    deploy: ed8de69f6b9741081b3bc70996ac7183d7594f43

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.sttk.stringcase","l":"StringCase"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.sttk.stringcase","l":"Options"},{"p":"com.github.sttk.stringcase","l":"StringCase"}];updateSearchResults();
