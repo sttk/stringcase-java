ad5f2cfa4442335b6b2e1a7e01288982bebf079f
sttk <sttk.xslet@gmail.com>
2026-08-12 03:05:00 +0900

    comment: updated copyright year in Options.java

--- src/main/java/com/github/sttk/stringcase/Options.java
+++ src/main/java/com/github/sttk/stringcase/Options.java
@@ -1,6 +1,6 @@
 /*
  * Options class.
- * Copyright (C) 2025 Takayuki Sato. All Rights Reserved.
+ * Copyright (C) 2025-2026 Takayuki Sato. All Rights Reserved.
  */
 package com.github.sttk.stringcase;
 
